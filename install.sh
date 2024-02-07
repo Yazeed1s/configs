2cb08ffc530f57049b048b3f662456432416a238
Yazeed1s <yazeed4n@gmail.com>
2024-02-07 14:09:50 -0500

    cleanup

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@
 # neovim: a text editor
 # vim: another text editor
 # vscodium-bin: another text editor
+# emacs: another text editor
 # nitrogen: wallpaper switcher
 # ttf-jetbrains-mono: JetBrains Mono font
 # ttf-jetbrains-mono-nerd: Some nerd fonts for icons and overall look
@@ -67,67 +68,74 @@
 # ufw: firewall rules manger
 # mariadb/mysql: MariaDB is the default implementation of MySQL in Arch Linux
 # postgresql: postgresql db
-# fastfetch: neofetch for sys-info
+# fastfetch(ignored): neofetch for sys-info
+# fd: GNU's find alternative
+# ripgrep: GNU's grep alternative
+# gdu: ncdu but faster
 
 INSTALLERDIR=$(dirname "$0")
 
 pacman_packages=(
-	fastfetch picom polybar sxhkd bspwm 
-	rofi dmenu btop nemo alacritty 
-    	firefox neovim vim nitrogen 
-	brightnessctl gvfs bluez bluez-utils 
-	lxappearance go rust cmake 
-	clang grpc protobuf pavucontrol 
-	openssh dmidecode zsh amd-ucode 
-	arandr pulseaudio alsa-utils 
-	xf86-video-qxl xorg xorg-xinit 
-	pacman-contrib git mesa
-	base-devel networkmanager wpa_supplicant
-	wireless_tools netctl dialog lvm2
+	picom polybar sxhkd
+	bspwm rofi dmenu
+	btop nemo alacritty
+	firefox neovim vim
+	nitrogen brightnessctl gvfs
+	bluez bluez-utils lxappearance
+	go rust cmake
+	clang grpc protobuf
+	pavucontrol openssh dmidecode
+	zsh amd-ucode arandr
+	pulseaudio alsa-utils xf86-video-qxl
+	xorg xorg-xinit pacman-contrib
+	git mesa base-devel
+	networkmanager wpa_supplicant wireless_tools
+	netctl dialog lvm2
 	rsync tmux fzf
+	emacs fd ripgrep gdu
 )
 
 # Install packages with pacman
 echo -e "Installing packages with pacman...\n"
 if ! sudo pacman -S "${pacman_packages[@]}"; then
-    echo -e "Error: pacman installation failed. Exiting script.\n"
-    exit 1
+	echo -e "Error: pacman installation failed. Exiting script.\n"
+	exit 1
 fi
 
 # start the SSH server
-read -n1 -rep 'would you like to start SSH server? (y,n)' HYP
-if [[ $HYP == "Y" || $HYP == "y" ]]; then
+read -n1 -rep 'would you like to start SSH server? (y,n)' ANS
+if [[ $ANS == "Y" || $ANS == "y" ]]; then
 	echo -e "Starting SSH server...\n"
 	if sudo systemctl enable --now sshd; then
-    		echo -e "SSH server started successfully.\n"
-    		sleep 3
+		echo -e "SSH server started successfully.\n"
+		sleep 3
 	else
-    		echo -e "Error: Failed to start the SSH server. Exiting script.\n"
-    		exit 1
+		echo -e "Error: Failed to start the SSH server. Exiting script.\n"
+		exit 1
 	fi
 fi
 
 # start NetworkManager
 echo -e "Starting NetworkManager....\n"
 if sudo systemctl enable --now NetworkManager; then
-    echo -e "NetworkManager started successfully.\n"
-    sleep 3
+	echo -e "NetworkManager started successfully.\n"
+	sleep 3
 else
-    echo -e "Error: Failed to start NetworkManager. Exiting script.\n"
-    exit 1
+	echo -e "Error: Failed to start NetworkManager. Exiting script.\n"
+	exit 1
 fi
 
 # Start MySQL (MariaDB)
-read -n1 -rep 'setup a MySQL (MariaDB) (y,n)' HYP
-if [[ $HYP == 'Y' || $HYP == 'y' ]]; then	
-	echo -e "Starting MariaDB...\n"	
+read -n1 -rep 'setup a MySQL (MariaDB) (y,n)' ANS
+if [[ $ANS == 'Y' || $ANS == 'y' ]]; then
+	echo -e "Starting MariaDB...\n"
 	if sudo pacman -S "mariadb"; then
 		if mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql && sudo systemctl enable mariadb.service; then
-    			echo -e "MariaDB started successfully.\n"
-    			sleep 3
+			echo -e "MariaDB started successfully.\n"
+			sleep 3
 		else
-    			echo -e "Error: Failed to start MariaDB. Exiting script.\n"
-    			exit 1
+			echo -e "Error: Failed to start MariaDB. Exiting script.\n"
+			exit 1
 		fi
 	else
 		echo -e "Error: Failed to install MariaDB. Exiting...\n"
@@ -135,26 +143,26 @@ if [[ $HYP == 'Y' || $HYP == 'y' ]]; then
 	fi
 fi
 
-# Start PostgreSQL 
-read -n1 -rep 'setup a PostgreSQL (y,n)' HYP
-if [[ $HYP == 'Y' || $HYP == 'y' ]]; then
+# Start PostgreSQL
+read -n1 -rep 'setup a PostgreSQL (y,n)' ANS
+if [[ $ANS == 'Y' || $ANS == 'y' ]]; then
 	echo -e "Starting PostgreSQL...\n"
 	if sudo pacman -S "postgresql"; then
 		# switch to the postgres user and initialize the data directory
 		if sudo -iu postgres && initdb -D /var/lib/postgres/data && exit; then
-    			echo -e "PostgreSQL initialized successfully.\n"
-    			sleep 3
+			echo -e "PostgreSQL initialized successfully.\n"
+			sleep 3
 			# enable the PostgreSQL service
 			if sudo systemctl enable postgresql; then
-    				echo -e "PostgreSQL service enabled successfully.\n"
+				echo -e "PostgreSQL service enabled successfully.\n"
 			else
-    				echo -e "Error: Failed to enable PostgreSQL service. Exiting script.\n"
-    			exit 1
+				echo -e "Error: Failed to enable PostgreSQL service. Exiting script.\n"
+				exit 1
 
 			fi
 		else
-    			echo -e "Error: Failed to initialize PostgreSQL. Exiting script.\n"
-    			exit 1
+			echo -e "Error: Failed to initialize PostgreSQL. Exiting script.\n"
+			exit 1
 		fi
 	fi
 fi
@@ -163,101 +171,101 @@ echo -e "Looking for yay...\n"
 sleep 2
 # check for yay and install if not found
 if ! command -v yay &>/dev/null; then
-    echo -e "yay not found. Installing yay...\n"
-    git clone https://aur.archlinux.org/yay.git
-    (cd yay && makepkg -si)
-    # check for yay installation success
-    if ! command -v yay &>/dev/null; then
-        echo -e "Error: yay installation failed. Exiting script.\n"
-        exit 1
-    fi
+	echo -e "yay not found. Installing yay...\n"
+	git clone https://aur.archlinux.org/yay.git
+	(cd yay && makepkg -si)
+	# check for yay installation success
+	if ! command -v yay &>/dev/null; then
+		echo -e "Error: yay installation failed. Exiting script.\n"
+		exit 1
+	fi
 else
-    echo -e "yay is already installed. Proceeding...\n"
+	echo -e "yay is already installed. Proceeding...\n"
 fi
-
 
 # install AUR packages with yay
 echo -e "Installing AUR packages with yay...\n"
 if ! yay -S \
-    vscodium-bin ttf-jetbrains-mono ttf-jetbrains-mono-nerd \
-    noto-fonts-emoji amdgpu_top vscodium-bin ttf-arabeyes-fonts gputest; then
-    echo -e "Error: AUR package installation failed. Exiting script.\n"
-    exit 1
+	vscodium-bin ttf-jetbrains-mono ttf-jetbrains-mono-nerd \
+	noto-fonts-emoji amdgpu_top vscodium-bin ttf-arabeyes-fonts gputest; then
+	echo -e "Error: AUR package installation failed. Exiting script.\n"
+	exit 1
 fi
 
 # copy config files
 echo -e "Copying config files...\n"
 config_dirs=(
 	"$INSTALLERDIR/.config/rofi"
-	"$INSTALLERDIR/.config/alacritty" 
-	"$INSTALLERDIR/.config/polybar" 
-	"$INSTALLERDIR/.config/bspwm" 
-	"$INSTALLERDIR/.config/sxhkd" 
-	"$INSTALLERDIR/.config/picom" 
+	"$INSTALLERDIR/.config/alacritty"
+	"$INSTALLERDIR/.config/polybar"
+	"$INSTALLERDIR/.config/bspwm"
+	"$INSTALLERDIR/.config/sxhkd"
+	"$INSTALLERDIR/.config/picom"
 	"$INSTALLERDIR/.config/btop"
+	"$INSTALLERDIR/.config/emacs"
+	"$INSTALLERDIR/.config/gdu"
 )
 for dir in "${config_dirs[@]}"; do
-    if ! cp -R "$dir" $HOME/.config/; then
-        echo -e "Error: Failed to copy $dir to ~/.config/. Exiting script.\n"
-        exit 1
-    fi
+	if ! cp -R "$dir" $HOME/.config/; then
+		echo -e "Error: Failed to copy $dir to ~/.config/. Exiting script.\n"
+		exit 1
+	fi
 done
 
 # Gruvbox Material GTK theme and icons
 echo "Installing Gruvbox Material GTK theme and icons..."
 # clone the Gruvbox Material GTK theme repository
 if ! git clone https://github.com/TheGreatMcPain/gruvbox-material-gtk.git $HOME/gruvbox-material-gtk; then
-    echo "Error: Failed to clone the repository. Exiting."
-    exit 1
+	echo "Error: Failed to clone the repository. Exiting."
+	exit 1
 fi
 
 # create ~/.icons and ~/.themes directories if they don't exist
 echo "Creating ~/.icons and ~/.themes directories..."
 if ! mkdir -p $HOME/.icons $HOME/.themes; then
-    echo "Error: Failed to create directories. Exiting."
-    exit 1
+	echo "Error: Failed to create directories. Exiting."
+	exit 1
 fi
 
 # copy theme files to ~/.themes
 if ! cp -r $HOME/gruvbox-material-gtk/themes/* $HOME/.themes; then
-    echo "Error: Failed to copy theme files. Exiting."
-    exit 1
+	echo "Error: Failed to copy theme files. Exiting."
+	exit 1
 fi
 
 # copy icon files to ~/.icons
 if ! cp -r $HOME/gruvbox-material-gtk/icons/* $HOME/.icons; then
-    echo "Error: Failed to copy icon files. Exiting."
-    exit 1
+	echo "Error: Failed to copy icon files. Exiting."
+	exit 1
 fi
 echo "Gruvbox Material GTK theme and icons installed successfully!"
 
 # Install the shell (oh-my-zsh)
 echo -e "Installing oh-my-zsh...\n"
 if ! sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; then
-    echo -e "Error: Oh-My-Zsh installation failed. Exiting script.\n"
-    exit 1
+	echo -e "Error: Oh-My-Zsh installation failed. Exiting script.\n"
+	exit 1
 fi
 
 # copy the specified dot files
 dot_files=("$INSTALLERDIR/.zshrc" "$INSTALLERDIR/.xinitrc" "$INSTALLERDIR/.tmux.conf")
 echo -e "Copying other dot files...\n"
 for file in "${dot_files[@]}"; do
-    if ! cp "$file" $HOME; then
-        echo -e "Error: Failed to copy $file. Exiting."
-        exit 1
-    fi
+	if ! cp "$file" $HOME; then
+		echo -e "Error: Failed to copy $file. Exiting."
+		exit 1
+	fi
 	if ! chmod +x $HOME/.xinitrc; then
-        echo -e "Error: failed to make $HOME/.xinitrc executable.\n"
+		echo -e "Error: failed to make $HOME/.xinitrc executable.\n"
 	fi
 done
-
 
 # Script is done
 echo -e "Script is completed.\n"
 echo -e "You can start bspwm by typing startx\n"
-read -n1 -rep 'Start now? (y,n)' HYP
-if [[ $HYP == "Y" || $HYP == "y" ]]; then
-    exec startx
+read -n1 -rep 'Start now? (y,n)' ANS
+if [[ $ANS == "Y" || $ANS == "y" ]]; then
+	exec startx
 else
-    exit
+	exit
 fi
